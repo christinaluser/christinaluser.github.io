848fcaf6b4955caf138d98b7df70013a0e255814
Christina Lu <christinalu42@gmail.com>
2022-02-23 01:20:42 -0700

    styling

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -29,30 +29,30 @@ for(let i = 1; i < 6; i++)
 
 
 
-const projects_observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    let nav_button = document.querySelector('.projects-nav');
-    if (entry.isIntersecting) {
-      nav_button.classList.add('nav-button-selected');
-      return;
-    }
-    nav_button.classList.remove('nav-button-selected');
-  });
-});
+// const projects_observer = new IntersectionObserver(entries => {
+//   entries.forEach(entry => {
+//     let nav_button = document.querySelector('.projects-nav');
+//     if (entry.isIntersecting) {
+//       nav_button.classList.add('nav-button-selected');
+//       return;
+//     }
+//     nav_button.classList.remove('nav-button-selected');
+//   });
+// });
 
-projects_observer.observe(document.querySelector('#projects'));
+// projects_observer.observe(document.querySelector('#projects'));
 
-const experience_observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    let nav_button = document.querySelector('.experience-nav');
-    if (entry.isIntersecting) {
-      nav_button.classList.add('nav-button-selected');
-      return;
-    }
+// const experience_observer = new IntersectionObserver(entries => {
+//   entries.forEach(entry => {
+//     let nav_button = document.querySelector('.experience-nav');
+//     if (entry.isIntersecting) {
+//       nav_button.classList.add('nav-button-selected');
+//       return;
+//     }
 
-    nav_button.classList.remove('nav-button-selected');
+//     nav_button.classList.remove('nav-button-selected');
     
-  });
-});
+//   });
+// });
 
-experience_observer.observe(document.querySelector('#experience'));
+// experience_observer.observe(document.querySelector('#experience'));
